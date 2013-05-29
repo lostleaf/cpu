3f5a13e6bbf5d5e357f8cde2d260c8e747699a1b
lostleaf <jack951753@gmail.com>
2013-05-29 23:13:34 +0800

    Finish alu and parameter

---
+++ alu.v
@@ -0,0 +1,35 @@
+module alu (op, in1, in2, clk, out, able)
+
+	`include "parameters.v"
+
+	input wire [3:0] op;
+	input wire clk, able;
+	input wire [WORD_SIZE-1:0] in1, in2;
+	output wire [WORD_SIZE-1:0] out;
+
+	always @(posedge clk) begin
+		if (able) begin
+            case (op)
+                ALU_ADD:
+                    out <= in1 + in2;
+                ALU_SUB:
+                    out <= in1 - in2;
+                ALU_MUL:
+                    out <= in1 * in2;
+                ALU_SLT:
+                    out <= in1 < in2;
+                ALU_AND:
+                    out <= in1 & in2;
+                ALU_OR:
+                    out <= in1 | in2;
+                ALU_XOR:
+                    out <= in1 ^ in2;
+                ALU_LSHIFT:
+                    out <= in1 << in2;
+                ALU_RSHIFT
+                	out <= in1 >> in2;
+            endcase
+		end
+	end
+
+endmodule
---
+++ parameters.v
@@ -0,0 +1,11 @@
+parameter WORD_SIZE = 32;
+
+parameter ALU_ADD       = 4'h0;
+parameter ALU_SUB       = 4'h1;
+parameter ALU_MUL       = 4'h2;
+parameter ALU_SLT       = 4'h3;
+parameter ALU_AND       = 4'h4;
+parameter ALU_OR        = 4'h5;
+parameter ALU_XOR       = 4'h6;
+parameter ALU_LSHIFT    = 4'h7;
+parameter ALU_RSHIFT    = 4'h8;
