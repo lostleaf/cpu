489ca892d0b54325accb88f2eddf5a835a15f3b6
lostleaf <jack951753@gmail.com>
2013-06-10 13:28:20 +0800

    update register file with luo xuan's version

--- ALU_RS.v
+++ ALU_RS.v
@@ -1,17 +1,17 @@
 module ALU_RS(fu, RB_index, instr, vj, vk, qj, qk, result, valid, reg_numj, reg_numk, busy, reset, clk);
-	`include "parameters.v"
-	parameter op = ALU_ADD, stall = 1;		// able to be changed from outside
+    `include "parameters.v"
+    parameter op = ALU_ADD, stall = 1;      // able to be changed from outside
 
-	input	wire[FU_INDEX-1:0] 	fu;
-	input	wire[RB_INDEX-1:0] 	RB_index;
-	input	wire[WORD_SIZE-1:0] instr;
+    input   wire[FU_INDEX-1:0]  fu;
+    input   wire[RB_INDEX-1:0]  RB_index;
+    input   wire[WORD_SIZE-1:0] instr;
 
-	input	wire[WORD_SIZE-1:0]	vj, vk;
-	input	wire[FU_INDEX-1:0]	qj, qk;
-	output	reg [REG_INDEX-1:0]	reg_numj, reg_numk;
-	
-	output	reg [WORD_SIZE-1:0]	result;
-	output	reg 				valid, busy, reset, clk;
+    input   wire[WORD_SIZE-1:0] vj, vk;
+    input   wire[FU_INDEX-1:0]  qj, qk;
+    output  reg [REG_INDEX-1:0] reg_numj, reg_numk;
+    
+    output  reg [WORD_SIZE-1:0] result;
+    output  reg                 valid, busy, reset, clk;
 
 
 endmodule
