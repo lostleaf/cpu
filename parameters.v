0c84ecb11d6416fd41f572c8fcd8226aae9f7192
roxanneluo <mailzhuce310@sina.com>
2013-06-23 13:36:40 +0800

    datacache and parameters changed

--- parameters.v
+++ parameters.v
@@ -4,8 +4,10 @@ parameter REG_INDEX     = 5;
 parameter REG_FILE_SIZE = 32;
 
 parameter BLOCK_SIZE    = 16;
+parameter BLOCK_INDEX	= 4;
 parameter CACHE_SIZE    = 1024;
-parameter TAG_SIZE      = 18;
+parameter CACHE_INDEX	= 10;
+parameter TAG_SIZE      = WORD_SIZE-CACHE_INDEX-BLOCK_INDEX;
 
 parameter ALU_ADD       = 4'h0;
 parameter ALU_SUB       = 4'h1;
